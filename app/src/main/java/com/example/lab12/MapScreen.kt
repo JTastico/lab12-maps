68bdb362c34d54cf3505831429649335817280e6
JTastico <jamilturparocutipa@gmail.com>
2024-11-15 08:48:46 -0500

    add zoom to Yura

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -1,38 +1,47 @@
 package com.example.lab12
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Marker
-import android.graphics.Bitmap
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun MapScreen() {
     val locations = listOf(
-        LatLng(-16.433415,-71.5442652), // JLByR
-        LatLng(-16.4205151,-71.4945209), // Paucarpata
-        LatLng(-16.3524187,-71.5675994) // Zamacola // Arequipa, Perú
+        LatLng(-16.433415, -71.5442652), // JLByR
+        LatLng(-16.4205151, -71.4945209), // Paucarpata
+        LatLng(-16.3524187, -71.5675994)  // Zamacola
     )
     val context = LocalContext.current
-    //val iconBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.mountain)
-    // Redimensiona el icono a un tamaño más pequeño (ajusta el tamaño según necesites)
-    //val scaledBitmap = Bitmap.createScaledBitmap(iconBitmap, 100, 100, false)
 
+    val cameraPositionState = rememberCameraPositionState()
+
+    // Animar la cámara al abrir el mapa
+    LaunchedEffect(Unit) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984, -71.6836503), 12f), // Centrar en Yura
+            durationMs = 3000 // Duración de la animación
+        )
+    }
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+            // Añadir marcadores en las ubicaciones
             locations.forEach { location ->
                 Marker(
                     state = rememberMarkerState(position = location),
@@ -42,4 +51,4 @@ fun MapScreen() {
             }
         }
     }
-    }
+}
